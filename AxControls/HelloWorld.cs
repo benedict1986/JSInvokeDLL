98b83c87524859040510fba724292f04c8838297
Ben <benedict1986@gmail.com>
2015-11-07 23:07:06 +1100

    Implement IObjectSafety interface in HelloWorld.cs

--- AxControls/HelloWorld.cs
+++ AxControls/HelloWorld.cs
@@ -13,11 +13,32 @@ namespace AxControls
     [Guid("7442B95D-9F7D-422C-9C87-EF01FF042106")]
     [ProgId("AxControls.HelloWorld")]
     [ComDefaultInterface(typeof(IHelloWorld))]
-    public class HelloWorld : UserControl, IHelloWorld
+    public class HelloWorld : UserControl, IHelloWorld, IObjectSafety
     {
         public string GetText()
         {
             return "Hello ActiveX World!";
+        }
+
+        public enum ObjectSafetyOptions
+        {
+            INTERFACESAFE_FOR_UNTRUSTED_CALLER = 0x00000001,
+            INTERFACESAFE_FOR_UNTRUSTED_DATA = 0x00000002,
+            INTERFACE_USES_DISPEX = 0x00000004,
+            INTERFACE_USES_SECURITY_MANAGER = 0x00000008
+        };
+
+        public int GetInterfaceSafetyOptions(ref Guid riid, out int pdwSupportedOptions, out int pdwEnabledOptions)
+        {
+            ObjectSafetyOptions m_options = ObjectSafetyOptions.INTERFACESAFE_FOR_UNTRUSTED_CALLER | ObjectSafetyOptions.INTERFACESAFE_FOR_UNTRUSTED_DATA;
+            pdwSupportedOptions = (int)m_options;
+            pdwEnabledOptions = (int)m_options;
+            return 0;
+        }
+
+        public int SetInterfaceSafetyOptions(ref Guid riid, int dwOptionSetMask, int dwEnabledOptions)
+        {
+            return 0;
         }
 
     }
