5d06f2a90611f5b4370c5874405547d2997a57df
Ben <benedict1986@gmail.com>
2015-11-07 22:48:48 +1100

    1.Rename 'Class1.cs' to 'HelloWorld.cs', making sure to rename the class name as well. 2.Add a project reference to System.Windows.Forms. 3.Make the HelloWorld class inherit UserControl.

--- AxControls/HelloWorld.cs
+++ AxControls/HelloWorld.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AxControls
 {
-    public class Class1
+    public class HelloWorld : UserControl
     {
     }
 }
