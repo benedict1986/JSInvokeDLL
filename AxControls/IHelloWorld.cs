de10b80e8a645cb38aeaabd81b54a3fffb5daa54
Ben <benedict1986@gmail.com>
2015-11-07 22:55:25 +1100

    Add the following to IHelloWorld.cs 1. [ComVisible(true)] makes the interface visible to COM.  2. [InterfaceType(ComInterfaceType.InterfaceIsDual)] sets the COM interface type to Dual, see InterfaceTypeAttribute Class on MSDN.  3. [Guid("05E5F955-14EF-4080-8740-4901EF6E74ED")] let's us manually assign a GUID to the interface. Use guidgen.exe or Tools -> Create GUID -> New GUID -> Copy in VS2015  to generate your own
    In IHelloWorld.cs, a function GetText() is added.

--- AxControls/IHelloWorld.cs
+++ AxControls/IHelloWorld.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AxControls
 {
+    [ComVisible(true)]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    [Guid("05E5F955-14EF-4080-8740-4901EF6E74ED")]
     public interface IHelloWorld
     {
+        string GetText();
     }
 }
