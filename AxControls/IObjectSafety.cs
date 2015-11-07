83210b4ec7a75d1047c5d900dec45cae836c3b85
Ben <benedict1986@gmail.com>
2015-11-07 23:25:25 +1100

    Before an ActiveX control can be used it must be installed and registered on the client. This can be done in a number of ways, from manually editing the registry to using regasm.exe, but we're going to create a Vistual Studio setup project to handle the installation for us.
    1.Right click the Visual Studio solution, select Add -> New Project and select Setup Project under Other Project Types. (If the project cannot be found, download and install Visual Studio Installer Projects extension from Tools -> Extensions and Update)
    2.Call the project 'AxControlsInstaller' and click OK.
    3.Right click the 'AxControlsInstaller' project, select Add -> Project Output, select 'Primary output' from the 'AxControls'  project and click OK.
    4.Right click 'Primary output from AxControls (Active)' and select Properties.
    5.Change the Register property from 'vsdrpDoNotRegister' to 'vsdrpCOM'.
    6.Right click the 'AxControlsInstaller' project and select Build.
    
    If you meet a .NET framework does not match warning like "The version of the .NET Framework launch condition '.NET Framework 4' does not match the selected .NET Framework", select the installer project and click "Launch Conditions Editor" icon with a binoculars on top  or right click the project, select View -> Launch Conditions. Then click .NET Framework and change Version in Property windows to the same .NET Framework version as AxControl project.

--- AxControls/IObjectSafety.cs
+++ AxControls/IObjectSafety.cs
@@ -11,7 +11,11 @@ namespace AxControls
     [Guid("CB5BDC81-93C1-11CF-8F20-00805F2CD064")]
     [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
     public interface IObjectSafety
-    {     
+    {
+        [PreserveSig()]
+        int GetInterfaceSafetyOptions(ref Guid riid, out int pdwSupportedOptions, out int pdwEnabledOptions);
 
+        [PreserveSig()]
+        int SetInterfaceSafetyOptions(ref Guid riid, int dwOptionSetMask, int dwEnabledOptions);
     }
 }
