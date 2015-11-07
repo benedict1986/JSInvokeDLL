f51a07da5686060f442ba0548ca6eb40c6244fff
Ben <benedict1986@gmail.com>
2015-11-07 23:00:58 +1100

    We now have a COM visible control that implements the IHelloWorld interface.
    Add the follwing to HelloWorld.cs
    1. [ComVisible(true)] makes the control visible to COM, see ComVisibleAttribute Class on MSDN.
     2. [ClassInterface(ClassInterfaceType.None)] indicates that no class interface is generated for this class, see ClassInterfaceType Enumeration on MSDN.
     3. [Guid("7442B95D-9F7D-422C-9C87-EF01FF042106")] let's us manually assign a GUID to the control, see GuidAttribute Class on MSDN. Use guidgen.exe to generate your own.
     4. [ProgId("AxControls.HelloWorld")] is a "user friendly" ID that we'll use later from JavaScript when initiating the control, see ProgIdAttribute Class on MSDN. You can define it by your own.
    [ComDefaultInterface(typeof(IHelloWorld))] sets IHelloWorld as the default interface that will be exposed to COM, see ComDefaultInterfaceAttribute Class on MSDN.

--- AxControls/HelloWorld.cs
+++ AxControls/HelloWorld.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AxControls
 {
-    public class HelloWorld : UserControl
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    [Guid("7442B95D-9F7D-422C-9C87-EF01FF042106")]
+    [ProgId("AxControls.HelloWorld")]
+    [ComDefaultInterface(typeof(IHelloWorld))]
+    public class HelloWorld : UserControl, IHelloWorld
     {
+        public string GetText()
+        {
+            return "Hello ActiveX World!";
+        }
+
     }
 }
