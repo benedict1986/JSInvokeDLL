80492094fd827ed2ca0d3994f569929fdb46e0c9
Ben <benedict1986@gmail.com>
2015-11-07 23:05:12 +1100

    By default IE will not allow initializing and scripting an ActiveX control unless it is marked as safe. This means that we won't be able to create instances of our ActiveX class with JavaScript by default. We can get around this by modifying the browser security settings, but a more elegant way would be to mark the control as safe. Before you do this to a "real" control, be sure to understand the consequences. I found an ancient (1996) MSDN article that explains this here. We will mark the control as safe by implementing the IObjectSafety interface.
    Create IObjectSafety interface.
    
    Add the following to the interface
    
    1. [ComImport()] IObjectSafety is a native interface so we have to redefine it for managed .Net use. This is done with the ComImport() attribute, see ComImportAttribute Class on MSDN.
     2. [Guid("CB5BDC81-93C1-11CF-8F20-00805F2CD064")] This is the GUID of the original IObjectSafety interface. Do not change it.
     3. [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)] sets the COM interface type to Unknown, see InterfaceTypeAttribute Class on MSDN.

---
+++ AxControls/IObjectSafety.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AxControls
+{
+    [ComImport()]
+    [Guid("CB5BDC81-93C1-11CF-8F20-00805F2CD064")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IObjectSafety
+    {     
+        [PreserveSig()]
+        int GetInterfaceSafetyOptions(ref Guid riid, out int pdwSupportedOptions, out int pdwEnabledOptions);
+ 
+        [PreserveSig()]
+        int SetInterfaceSafetyOptions(ref Guid riid, int dwOptionSetMask, int dwEnabledOptions);
+    }
+}
